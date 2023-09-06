8c85ba953e9473fcdac8d6eafb8a4978abf25ec8
dilaracelik73 <140396137+dilaracelik73@users.noreply.github.com>
2023-09-06 13:53:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, request, render_template
-import os
 from apı import get_weather_data, temperature_city_func, weather_desc_func, humidity_func, wind_speed_func, date_time_func
+#Yukarıda apı dosyasından fonksiyonlar import edilmiştir.
 
 app = Flask(__name__)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         location = request.form['location']
         api_data = get_weather_data(location)
 
+# apı dosyasındaki veriler orada return edilen api_data ile bağlantılı hale getirilmiştir.
         temperature = temperature_city_func(api_data)
         weather_description = weather_desc_func(api_data)
         humidity = humidity_func(api_data)
         wind_speed = wind_speed_func(api_data)
 
+# işlemler hatasız bir şekilde işlerse o zaman weather.html sayfasına yönlendirilme yapılacak ve bilgiler çekilecek.
         return render_template('weather.html',
                                location=location.title(),
                                date_time=date_time_func(),
